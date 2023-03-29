7fc97baafadc5791e5932dc9a67b3f78538a75dc
KaLong_desktop <kaxm462@gmail.com>
2023-03-30 00:21:58 +0200

    working code

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ MQTT_TOPIC = "example/topic"
 data = {"temperature": 25.5, "humidity": 60}
 
 # Convert the data to a string and send it via MQTT
-payload = str(data)
-publish.single(MQTT_TOPIC, payload, hostname=MQTT_BROKER)
-
-print("Data sent via MQTT.")
-
+while True:
+    payload = str(data)
+    publish.single(MQTT_TOPIC, payload, hostname=MQTT_BROKER)
+    print("Data sent via MQTT.")
+    time.sleep(5)
