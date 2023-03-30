13e1710ae589b9e9c9cf50f63a240aae508fbd4c
Ka Long Mok <kaxm462@gmail.com>
2023-03-30 21:12:38 +0200

    30-03-2023 new version

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import paho.mqtt.publish as publish
 import time
+import json
 
 # Set up the MQTT broker and topic
 MQTT_BROKER = "test.mosquitto.org"
-MQTT_TOPIC = "example/topic"
+MQTT_TOPIC = "AAS/data"
 
-# Define some data to send
-data = {"temperature": 25.5, "humidity": 60}
+# Define a list of data to send
+data_list = [2, 3, 4, 5, 6]
 
-# Convert the data to a string and send it via MQTT
+# Convert the data to a JSON string and send it via MQTT
 while True:
-    payload = str(data)
-    publish.single(MQTT_TOPIC, payload, hostname=MQTT_BROKER)
-    print("Data sent via MQTT.")
-    time.sleep(5)
+    for data in data_list:
+        payload = json.dumps([data])
+        publish.single(MQTT_TOPIC, payload, hostname=MQTT_BROKER)
+        print("Data sent via MQTT:", data)
+        time.sleep(2)
